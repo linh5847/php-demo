4e1a8e60d4d8a1e2c902c2a7bae78281b7df943c
linh5847 <52471472+linh5847@users.noreply.github.com>
2025-06-06 11:33:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,9 @@ $database = "phpdb";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
 
+mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+mysqli_real_connect($conn, 'phpdb.mysql.database.azure.com', 'dbadmin@phpdb', 'M!ke0rBr$ak1234', 'quickstartdb', 3306, MYSQLI_CLIENT_SSL);
+
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
