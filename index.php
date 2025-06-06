2048f32ec5e24eefeaf3871526cd1004cefb7f81
linh5847 <52471472+linh5847@users.noreply.github.com>
2025-06-06 11:13:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-  <?php echo("Hello World"); ?>
+//  <?php echo("Hello World"); ?>
+<?php
+$servername = "phpdb.mysql.database.azure.com";
+$username = "dbadmin";
+$password = "M!ke0rBr$ak1234";
+$database = "phpdb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected to the database successfully!";
+
+// Perform database operations here...
+
+// Close the connection
+$conn->close();
+?>
